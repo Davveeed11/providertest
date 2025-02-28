3fc55eb519076c32c121961dfeb63d44a55d83be
unknown <drizzy2908@gmail.com>
2025-02-28 10:21:55 +0000

    Initial commit

---
+++ lib/feature/data/home_Model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeModel extends ChangeNotifier{
+  int start = 0;
+
+  void increment(){
+    start++;
+    notifyListeners();
+  }
+
+  void decrement(){
+    start++;
+    notifyListeners();
+  }
+}
